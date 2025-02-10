4aa9ed843060eb2faf62d7e81ffa64131d995736
Emily Wade <emilywade314@gmail.com>
2025-02-10 15:58:41 +0000

    adding playerTurn function and test

--- scripts/game.js
+++ scripts/game.js
@@ -53,5 +53,16 @@ function showTurns() {
     }, 800);
 }
 
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length == game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    }
+}
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
